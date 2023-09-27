49be8d80871335e9ff3d13c93813688d7657a286
Yagnesh Vakharia <yagneshvakharia97@gmail.com>
2023-09-27 18:12:49 +0530

    added caching

--- app/utilities/config.py
+++ app/utilities/config.py
@@ -1,16 +1,21 @@
+# mypy: ignore_errors
 from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     env: str = "staging"
+    mongo_uri: str
+    mongo_db: str
     token: str
     graph_url: str = "https://graph.facebook.com/v17.0"
     graph_token: str
     page_name: str
-    
+
     bing_url: str = "https://api.bing.microsoft.com/v7.0"
     bing_token: str
 
     class Config:
-        env_file = '.env'
+        env_file = ".env"
+
 
 settings = Settings()
