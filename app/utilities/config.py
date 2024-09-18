a32355ffc9ecc085608ddd094f4b658b140a2822
Yagnesh Vakharia <yagneshvakharia97@gmail.com>
2024-09-19 01:16:52 +0530

    added fb page id and genai model and api key

--- app/utilities/config.py
+++ app/utilities/config.py
@@ -6,12 +6,16 @@ class Settings(BaseSettings):
     env: str = "staging"
     mongo_uri: str
     mongo_db: str
+
     graph_url: str = "https://graph.facebook.com/v20.0"
     graph_token: str
-    page_name: str
+    page_id: int
 
     bing_url: str = "https://api.bing.microsoft.com/v7.0"
     bing_token: str
+
+    genai_model: str = "gemini-1.5-flash"
+    genai_api_key: str
 
     class Config:
         env_file = ".env"
